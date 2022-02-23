efc87d70e59e9709ea65b0c42db57dd93afb7226
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-02-24 08:29:25 +0900

    chapter5-1-5完了

---
+++ honkaku/practice/chapter5/method/items.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>GETメソッド</title>
+</head>
+
+<body>
+    <h3>創作したカテゴリーは<?= $_GET['category'] ?>です</h3>
+</body>
+
+</html>
---
+++ honkaku/practice/chapter5/request/thankyou.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>入力フォーム - honkaku</title>
+</head>
+
+<body>
+    <h2>選択された地域の番号は、<?= $_REQUEST['area'] ?>です。</h2>
+</body>
+
+</html>
